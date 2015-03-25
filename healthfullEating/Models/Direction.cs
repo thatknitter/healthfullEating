6c64f4421ba7261840186226cae63003ce2fbd06
thatknitter <kblewis.flt@gmail.com>
2015-03-25 14:16:09 -0500

    Direction model binding

--- healthfullEating/Models/Direction.cs
+++ healthfullEating/Models/Direction.cs
@@ -7,11 +7,13 @@ namespace healthfullEating
 	{
 		public int ID { get; set; }
 		public int RecipeID { get; set; }
-		public string Info { get; set; } 
+		public string Info { get; set; }
 
-		public Direction ()
+        public virtual Recipe Recipe { get; set; }
+
+		public Direction (string Dir)
 		{
-			
+            this.Info = Dir;
 		}
 	}
 
